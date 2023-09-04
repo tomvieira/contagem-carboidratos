f92e750321e89c68ac3d6a3a0aad804aa543d216
davifelipef <feliperedfield@gmail.com>
2023-09-04 18:49:03 -0300

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,36 +1,130 @@
 // Carrega o script depois que todos os elementos da página são carregados
 document.addEventListener('DOMContentLoaded', function() {
+    // Cria a lista de itens
+    let cartList = []; 
+    // Cria o contador de itens da lista
+    let cartCount = 0; 
+    // Declara a array para uso imediato
+    let dataArray = [];
+    // Atribui os elementos HTML a constantes específicas
+    const listContainer = document.getElementById('listContainer');
+    const cartIcon = document.getElementById('cart');
+    const cartItemsList = document.getElementById('cartItemsList');
+
     // Chama a função que preenche as informações assim que a página carrega
     window.onload = function() {
-        //console.log('Função onload iniciada'); // Log de debug
-        // Atribui o elemento HTML especificado à constante
+        // Atribui o elemento HTML à constante
         const selectElement = document.getElementById('foodList');
         const selectedOption = selectElement.options[selectElement.selectedIndex];
         const selectedOptionId = selectedOption.id;
-        
-        //console.log('ID da opção selecionada: ' + selectedOptionId); // Log de debug
-
         // Chama a função com o ID da opção selecionada
         fetchDataAndPopulateList(selectedOptionId);
     }
 
     // Adiciona um event listener para detectar mudança na lista de tipo de alimentos
     document.getElementById('foodList').addEventListener('change', function() {
-        //console.log('Mudança detectada.') // Log de debug
-        // Atribui o elemento HTML especificado à constante
         const selectElement = document.getElementById('foodList');
         const selectedOption = selectElement.options[selectElement.selectedIndex];
         const selectedOptionId = selectedOption.id;
-        
-        // Chama a função que preenche a segunda lista com base na opção selecionada na primeira lista
         fetchDataAndPopulateList(selectedOptionId);
     });
 
+    // Inicia a constante atribuída ao botão de adicionar itens ao carrinho
+    const addButton = document.getElementById('addButton');
+    addButton.addEventListener('click', () => {
+        const selectedOptionIndex = listContainer.selectedIndex;
+        const selectedOptionData = dataArray[selectedOptionIndex];
+        const carbsValue = document.getElementById('carbs').textContent; 
+        // Cria um objeto com o nome do item e o valor do carboidrato
+        const cartItem = {
+            item: selectedOptionData.item,
+            carbValue: carbsValue,
+        };
+        // Adiciona o objeto criado acima à lista
+        cartList.push(cartItem);
+        // Soma 1 na contagem de itens do carrinho
+        cartCount++;
+        // Chama a função que atualiza os itens do carrinho
+        updateCartDisplay();
+    });
+
+    // Adiciona um event listener para detectar quando há clique no carrinho
+    cartIcon.addEventListener('click', () => {
+        const modal = document.getElementById('cartModal');
+        // Se a tela modal estiver sendo exibida, a fecha
+        if (modal.style.display === 'block') {
+            closeCartModal();
+        // se não, abre a tela modal
+        } else {
+            openCartModal();
+        }
+    });
+
+    // Adiciona um event listener para detectar quando há clique no botão de fechar a lista
+    const closeButton = document.querySelector('.close');
+    closeButton.addEventListener('click', () => {
+        closeCartModal();
+    });
+
+    // Função que abre a janela modal com a lista de itens
+    function openCartModal() {
+        const modal = document.getElementById('cartModal');
+        modal.style.display = 'block';
+
+        // Limpa os itens do carrinho antes de populá-lo
+        cartItemsList.innerHTML = '';
+        // Chama a função que popula o carrinho
+        populateCartItems();
+    }
+
+    // Função que fecha a janela modal com a lista de itens    
+    function closeCartModal() {
+        const modal = document.getElementById('cartModal');
+        modal.style.display = 'none';
+    }
+
+    // Função que atualiza a contagem de itens do carrinho
+    function updateCartDisplay() {
+        const cartItemsElement = document.querySelector('.cartItems');
+        cartItemsElement.textContent = cartCount;
+    }
+
+    // Função que popula o carrinho
+    function populateCartItems() {
+        // Limpa os itens anteriores
+        cartItemsList.innerHTML = ''; 
+        // Inicializa a variável que contabiliza o total de carboidratos
+        let totalCHO = 0; 
+        
+        // Para cada item da lista de itens
+        cartList.forEach((item, index) => {
+            // Cria uma div
+            const cartItemDiv = document.createElement('div');
+            // Transforma o valor dos carboidratos em uma integer
+            const choValue = parseInt(item.carbValue); 
+            // Adiciona o valor ao total
+            totalCHO += choValue; 
+            // Exibe um texto com o nome e o valor do carboidrato do item
+            cartItemDiv.textContent = `${item.item}: ${item.carbValue}`; 
+            // Adiciona um item embaixo do outro
+            cartItemsList.appendChild(cartItemDiv);
+    
+            // Adiciona espaço vertical entre o último item da lista e o total
+            if (index === cartList.length - 1) {
+                cartItemDiv.style.marginBottom = '1.5em'; 
+            }
+        });
+    
+        // Cria um div para o total e adiciona como filho da lista
+        const totalDiv = document.createElement('div');
+        totalDiv.textContent = `Total: ${totalCHO} CHO`;
+        cartItemsList.appendChild(totalDiv);
+    }    
+
     async function fetchDataAndPopulateList(selectedOptionId) {
         try {
-            //console.log('Capturando e preenchendo a lista para a opção: ' + selectedOptionId); // Log de debug
-            let csvFilePath = ''; // Inicializa o caminho do arquivo CSV dependendo da opção selecionada
-
+            // Atribui uma variável para o arquivo CSV
+            let csvFilePath = '';
             // Determina o acesso ao caminho do arquivo CSV dependendo da opção selecionada
             switch (selectedOptionId) {
                 case 'tb1': // Tabela padrão
@@ -83,95 +177,64 @@ document.addEventListener('DOMContentLoaded', function() {
                     return; // Sai da função se for selecionada uma opção desconhecida
             }
 
+            // Espera que o arquivo CSV seja determinado
             const response = await fetch(csvFilePath);
             const data = await response.text();
-
             const lines = data.split('\n');
-            const dataArray = [];
+            dataArray = [];
 
             for (let i = 1; i < lines.length; i++) {
                 const line = lines[i];
                 const items = parseCSVLine(line);
 
+                // Se houver um item
                 if (items.length > 0) {
-                    // Constante que captura o nome do item do arquivo CSV selecionado
+                    // nome do item
                     const item = items[0];
-                    // Constante que captura a medida usual do item do arquivo CSV selecionado
+                    // medida usual
                     const measure = items[1];
-                    // Constante que captura o peso do item do arquivo CSV selecionado
+                    // peso
                     const weight = items[2];
-                    // Constante que captura o valor do carboidrato (CHO) do item do arquivo CSV selecionado
+                    // CHO
                     const carb = items[3];
-                    // Armazena os valores dos items e dos carboidreatos na array
+                    // Armazena as informações na array
                     dataArray.push({ item, measure, weight, carb });
                 }
             }
-            // Atribui o elemento HTML especificado à constante
-            const listContainer = document.getElementById('listContainer');
-            // Limpa as opções anteriores
-            listContainer.innerHTML = ''; 
-            // Para cada item da array
+
+            listContainer.innerHTML = '';
             dataArray.forEach(itemData => {
-                // Cria uma opção dentro da tag select
                 const option = document.createElement('option');
-                // Adicionando o nome do item
                 option.textContent = `${itemData.item}`;
-                // Ao container, como seu filho
                 listContainer.appendChild(option);
-                // Atribui o elemento carbs a uma constante
-                const carbsParagraph = document.getElementById('carbs');
-                // Atribui o peso informado a uma constante
-                const inputWeight = document.getElementById('i_weight');
-                // Recupera a opção selecionada no container
+            });
+
+            const carbsParagraph = document.getElementById('carbs');
+            
+            // Calcula os carboidratos quando o número do peso é alterado
+            const inputWeight = document.getElementById('i_weight');
+            listContainer.addEventListener('change', () => {
                 const selectedOptionIndex = listContainer.selectedIndex;
-                // Verifica o index do item selecionado
                 const selectedOptionData = dataArray[selectedOptionIndex];
-                /* Atualiza o elemento carbs com o valor das calorias correspondente ao 
-                index do item selecionado*/
                 let result = (inputWeight.value * parseFloat(selectedOptionData.carb)) / parseFloat(selectedOptionData.weight);
-                // arredonda o resultado
                 result = Math.ceil(result);
-                // Atualiza o elemento com base no elemento selecionado na segunda lista
-                carbsParagraph.textContent = `${result} CHO`;
-                //console.log(`${result} CHO`); // Log de debug
-                
-            // Adiciona um event listener que detecta mudanças na lista de alimentos
-            listContainer.addEventListener('change', (event) => {
-                // Recupera a opção selecionada no container
-                const selectedOptionIndex = event.target.selectedIndex;
-                // Verifica o index do item selecionado
-                const selectedOptionData = dataArray[selectedOptionIndex];
-                // Calcula o resultado seguindo a regra da fórmula de três
-                let result = (inputWeight.value * parseFloat(selectedOptionData.carb)) / parseFloat(selectedOptionData.weight);
-                // Arredonda o resultado
-                result = Math.ceil(result);
-                // Atualiza o elemento com base no elemento selecionado na segunda lista
                 carbsParagraph.textContent = `${result} CHO`;
             });
 
-            // Adiciona um event listener que detecta mudanças no peso do alimento
-            inputWeight.addEventListener('input', (event) => {
-                //console.log('Input weight changed'); // Log de debug
-                // Recupera a opção selecionada no container
+            // Recalcula os carboidratos quando muda o número do peso
+            inputWeight.addEventListener('input', () => {
                 const selectedOptionIndex = listContainer.selectedIndex;
-                // Verifica o index do item selecionado
                 const selectedOptionData = dataArray[selectedOptionIndex];
-                // Recalcula o resultado seguindo a regra da fórmula de três
                 let result = (inputWeight.value * parseFloat(selectedOptionData.carb)) / parseFloat(selectedOptionData.weight);
-                // Arredonda o resultado
                 result = Math.ceil(result);
-                // Atualiza o elemento com base no elemento selecionado na segunda lista
                 carbsParagraph.textContent = `${result} CHO`;
-            });
+            });            
 
-        });
-        // Lida com erros
         } catch (error) {
             console.error('Erro buscando ou carregando os dados:' + error);
         }
     }
-    
-    // Função que formata o texto do item no caso de uso de aspas duplas
+
     function parseCSVLine(line) {
         const items = [];
         let currentItem = '';
@@ -190,4 +253,5 @@ document.addEventListener('DOMContentLoaded', function() {
         items.push(currentItem.trim());
         return items;
     }
+
 });
